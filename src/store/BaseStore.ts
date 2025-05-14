a1cfdb5d33c6900fecf1e3952203951386e1dc4a
Doubley Yang <doubley_yang@epam.com>
2025-05-14 15:41:15 +0800

    feat: add unit type for the local store type

--- src/store/BaseStore.ts
+++ src/store/BaseStore.ts
@@ -18,11 +18,15 @@ export interface AsyncLocalStorageStore {
 export interface CreateOptions {
   type: 'file' | 'directory',
 }
+export interface GetOptions {
+  start: number,
+  end: number,
+}
 
 export interface StatResult {
   mtime: Date,
   size: number,
-  mime: string,
+  mime: string | null,
   type: 'file' | 'directory'
 }
 
@@ -40,10 +44,14 @@ export abstract class BaseStore {
       device: this.device,
     }
   }
+  public check (targetPath: string) {
+    return decodeURIComponent(targetPath).startsWith(this.path) // && this.device.filter
+  }
   public abstract has (targetPath: string): Promise<boolean>
   public abstract create (targetPath: string, options: CreateOptions): Promise<boolean>
-  public abstract delete (targetPath: string): Promise<unknown>
-  public abstract get (targetPath: string): Promise<fs.ReadStream>
+  public abstract update (targetPath: string, content: string | fs.ReadStream): Promise<unknown>
+  public abstract delete (targetPath: string): Promise<boolean>
+  public abstract get (targetPath: string, options: GetOptions): Promise<fs.ReadStream>
   public abstract stat (targetPath: string): Promise<StatResult>
   public abstract lock (targetPath: string): Promise<unknown>
   public abstract unlock (targetPath: string): Promise<unknown>
--- src/store/LocalStore.ts
+++ src/store/LocalStore.ts
@@ -1,7 +1,8 @@
 import path from 'path'
 import fs from 'fs'
-import { BaseStore, CreateOptions, StatResult, Store } from "./BaseStore"
 import { rimrafSync } from 'rimraf'
+import mime from 'mime'
+import { BaseStore, CreateOptions, GetOptions, StatResult, Store } from "./BaseStore"
 
 export class LocalStore extends BaseStore {
   constructor (webPath: Store['path'], options: { path: string, filter?: string }) {
@@ -17,11 +18,11 @@ export class LocalStore extends BaseStore {
     return new LocalStore(...args)
   }
   public async has (targetPath: string) {
-    const currentPath = path.join(this.device.path, targetPath)
+    const currentPath = this.getResourcePath(targetPath)
     return fs.existsSync(currentPath)
   }
   public async create(targetPath: string, options: CreateOptions): Promise<boolean> {
-    const currentPath = path.join(this.device.path, targetPath)
+    const currentPath = this.getResourcePath(targetPath)
     switch (options.type) {
       case 'directory':
         fs.mkdirSync(currentPath, { recursive: true })
@@ -35,30 +36,41 @@ export class LocalStore extends BaseStore {
     }
   }
   public async delete(targetPath: string) {
-    const currentPath = path.join(this.device.path, targetPath)
+    const currentPath = this.getResourcePath(targetPath)
     rimrafSync(currentPath)
+    return true
   }
-  public async get(targetPath: string) {
-    const currentPath = path.join(this.device.path, targetPath)
-    return fs.createReadStream(currentPath)
+  public async get(targetPath: string, options?: GetOptions) {
+    const currentPath = this.getResourcePath(targetPath)
+    const readStreamOptions = options ? { start: options.start, end: options.end } : undefined
+    return fs.createReadStream(currentPath, readStreamOptions)
+  }
+  public async update(targetPath: string, content: string | fs.ReadStream) {
+    const currentPath = this.getResourcePath(targetPath)
+    if (typeof content === 'string') {
+      fs.writeFileSync(currentPath, content, 'utf-8')
+    }
   }
   public async stat (targetPath: string) {
-    const currentPath = path.join(this.device.path, targetPath)
+    const currentPath = this.getResourcePath(targetPath)
     const stat = fs.statSync(currentPath)
     const result: StatResult =  {
       mtime: stat.mtime,
-      size: stat.size,
-      mime: '',
+      size: stat.isFile() ? stat.size : 0,
+      mime: mime.getType(currentPath),
       type: stat.isFile() ? 'file' : 'directory',
     }
     return result
   }
   public async lock(targetPath: string): Promise<unknown> {
-    const currentPath = path.join(this.device.path, targetPath)
+    const currentPath = this.getResourcePath(targetPath)
     throw new Error('Method not implemented.');
   }
   public async unlock(targetPath: string): Promise<unknown> {
-    const currentPath = path.join(this.device.path, targetPath)
+    const currentPath = this.getResourcePath(targetPath)
     throw new Error('Method not implemented.');
+  }
+  private getResourcePath (targetPath: string) {
+    return path.join(this.device.path, decodeURIComponent(targetPath))
   }
 }
